f289a1ded29cc6112d81cd9c43cf2ccd1117a313
肖智超 <421146350@qq.com>
2019-11-03 21:01:07 +0800

    添加swagger注解

--- douyin/douyin/douyin-pojo/src/main/java/com/xzc/pojo/Users.java
+++ douyin/douyin/douyin-pojo/src/main/java/com/xzc/pojo/Users.java
@@ -1,25 +1,37 @@
 package com.xzc.pojo;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import java.io.Serializable;
 import java.util.Date;
 /***
 *   
 */
+@ApiModel(value = "user对象",description = "user对象")
 public class Users implements Serializable {
         //
+        @ApiModelProperty(hidden = true)
             private String id;
         //用户名
+        @ApiModelProperty(value = "用户名",name = "username",required = true)
             private String username;
         //密码
+        @ApiModelProperty(value = "密码",name = "password",required = true)
             private String password;
         //我的头像，如果没有默认给一张
+        @ApiModelProperty(hidden = true)
             private String faceImage;
         //昵称
+        @ApiModelProperty(value = "昵称",name = "nickname")
             private String nickname;
         //我的粉丝数量
+        @ApiModelProperty(hidden = true)
             private Integer fansCounts;
         //我关注的人总数
+        @ApiModelProperty(hidden = true)
             private Integer followCounts;
         //我接受到的赞美/收藏 的数量
+        @ApiModelProperty(hidden = true)
             private Integer receiveLikeCounts;
         //get set 方法
             public void setId (String  id){
