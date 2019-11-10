d946c82543cf40af0c18a1a33d02a9fac8d709a5
肖智超 <421146350@qq.com>
2019-11-10 11:36:34 +0800

    注册和配置了拦截器

--- douyin-controller-api/src/main/java/com/xzc/config/WebMvcConfig.java
+++ douyin-controller-api/src/main/java/com/xzc/config/WebMvcConfig.java
@@ -1,6 +1,9 @@
 package com.xzc.config;
 
+import com.xzc.controller.interceptor.MiniInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -19,5 +22,26 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/META-INF/resources/")
                 .addResourceLocations("file:H:\\gitReponsitory\\douyin\\douyin_userFiles\\");//映射文件夹
+    }
+
+
+    /**
+     * 注册拦截器
+     * @return
+     */
+    @Bean
+    public MiniInterceptor miniInterceptor(){
+        return new MiniInterceptor();
+    }
+
+    /**
+     * 添加拦截器 alt+shift+s override addInterceptors
+     * @param registry
+     */
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        super.addInterceptors(registry);
+        registry.addInterceptor(miniInterceptor()).addPathPatterns("/user/**");//填写拦截路径,接口路径
+
     }
 }
