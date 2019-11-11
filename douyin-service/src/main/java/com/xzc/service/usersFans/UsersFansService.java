93015bf800c0e4cbd5324c14f8f989c3955dd0b7
肖智超 <421146350@qq.com>
2019-11-11 15:59:43 +0800

    删除粉丝，添加粉丝的业务逻辑实现

--- douyin-service/src/main/java/com/xzc/service/usersFans/UsersFansService.java
+++ douyin-service/src/main/java/com/xzc/service/usersFans/UsersFansService.java
@@ -12,18 +12,17 @@ package com.xzc.service.usersFans;
 
     public UsersFans getUsersFansById(Long id)throws Exception;
 
-    public List<UsersFans>    getUsersFansListByMap(Map
-                                                            <String, Object> param)throws Exception;
+    public List<UsersFans>    getUsersFansListByMap(Map<String, Object> param)throws Exception;
 
-    public Integer getUsersFansCountByMap(Map
-                                                  <String, Object> param)throws Exception;
+    public Integer getUsersFansCountByMap(Map<String, Object> param)throws Exception;
 
-    public Integer itriptxAddUsersFans(UsersFans usersFans)throws Exception;
+    public Integer itriptxAddUsersFans(String userId,String fansId)throws Exception;
 
     public Integer itriptxModifyUsersFans(UsersFans usersFans)throws Exception;
 
     public Integer itriptxDeleteUsersFansById(Long id)throws Exception;
 
-    public Page<UsersFans> queryUsersFansPageByMap(Map
-                                                           <String, Object> param, Integer pageNo, Integer pageSize)throws Exception;
-}
+    public Page<UsersFans> queryUsersFansPageByMap(Map<String, Object> param, Integer pageNo, Integer pageSize)throws Exception;
+
+    public void delUsersFans(String userId,String fansId)throws Exception;
+    }
--- douyin-service/src/main/java/com/xzc/service/usersFans/UsersFansServiceImpl.java
+++ douyin-service/src/main/java/com/xzc/service/usersFans/UsersFansServiceImpl.java
@@ -4,6 +4,7 @@ import com.xzc.pojo.UsersFans;
 /////
 import com.xzc.common.EmptyUtils;
 import com.xzc.common.Page;
+import org.n3r.idworker.Sid;
 import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -11,6 +12,9 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import com.xzc.common.Constants;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 @Service
 public class UsersFansServiceImpl implements UsersFansService {
 
@@ -29,8 +33,13 @@ public class UsersFansServiceImpl implements UsersFansService {
         return usersFansMapper.getUsersFansCountByMap(param);
     }
 
-    public Integer itriptxAddUsersFans(UsersFans usersFans)throws Exception{
-
+    @Transactional(propagation = Propagation.REQUIRED)//事务管理
+    public Integer itriptxAddUsersFans(String userId,String fansId)throws Exception{
+            UsersFans usersFans=new UsersFans();
+            String sid=new Sid().nextShort();
+            usersFans.setId(sid);
+            usersFans.setFanId(fansId);
+            usersFans.setUserId(userId);
             return usersFansMapper.insertUsersFans(usersFans);
     }
 
@@ -53,6 +62,12 @@ public class UsersFansServiceImpl implements UsersFansService {
         List<UsersFans> usersFansList = usersFansMapper.getUsersFansListByMap(param);
         page.setRows(usersFansList);
         return page;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)//事务管理
+    public void delUsersFans(String userId,String fansId)throws Exception{
+        //删除粉丝关系
+        usersFansMapper.delUsersFans(userId,fansId);
     }
 
 }
